7ecee7193fc7b70ffa315fac418fd50fad802949
monikaba <bamon9u@gmail.com>
2021-08-08 01:17:15 +0530

    added new information

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Achiever(models.Model):
+    first_name = models.CharField(max_length=20)
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.first_name
