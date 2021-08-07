ebca6ce69f558635c20dc83ca025cf89c6e16c47
jeethendra <jeethendrajeethu8@gmail.com>
2021-08-08 00:52:51 +0530

    added backend for achiever

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework import viewsets
+
+# our created models and serializer
+from . models import *
+from . serializers import *
 
 # Create your views here.
 def home(request):
     return HttpResponse("hi, this is home page of api app")
+
+# create a viewset
+class AchieversViewSet(viewsets.ModelViewSet):
+    # define queryset
+    queryset = Achievers.objects.all()
+     
+    # specify serializer to be used
+    serializer_class = AchieversSerializer
