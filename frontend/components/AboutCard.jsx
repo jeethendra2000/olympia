e34421d50966b24d8844650c7dfd5e4c0ee2628a
jeethendra <jeethendrajeethu8@gmail.com>
2021-08-08 18:08:51 +0530

    added contact form frontend

--- frontend/components/AboutCard.jsx
+++ frontend/components/AboutCard.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import Image from "next/image";
 import Avatar from "@material-ui/core/Avatar";
 import styles from "../styles/Home.module.css";
 
--- frontend/pages/contactUs.jsx
+++ frontend/pages/contactUs.jsx
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 
 function contactUs() {
-    return (
-        <div>
-            <h1>Contact us page</h1>
-        </div>
-    )
+  return (
+    <div style={{ marginTop: "50px" }}>
+      <Grid container spacing={4} alignItems="center">
+        <Grid item xs={12} sm={12} md={5}>
+          <Typography variant="h3" style={{ marginBottom: "20px" }}>
+            Contact Us
+          </Typography>
+          <Typography variant="h6" style={{ marginBottom: "40px" }}>
+            Feels like contacting us?
+            <br />
+            Submit your queries here and we will get back to you as soon as
+            possible.
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={2}></Grid>
+        <Grid item xs={12} sm={12} md={5}>
+          <Card elevation={8} style={{ borderRadius: "35px" }}>
+            <CardContent>
+              <Grid
+                justify="center"
+                alignItems="center"
+                direction="column"
+                container
+                spacing={3}
+                component="form"
+                noValidate
+                autoComplete="off"
+                // onSubmit={handleSubmit}
+              >
+                <Grid item>
+                  <Typography variant="h5" style={{ marginBottom: "20px" }}>
+                    Send us a message
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <form noValidate autoComplete="off">
+                    <TextField
+                      style={{
+                        margin: "10px",
+                        paddingRight: "20px",
+                      }}
+                      color="secondary"
+                      label="Full Name"
+                      variant="outlined"
+                      fullWidth
+                      required
+                    />
+                    <TextField
+                      style={{
+                        margin: "10px",
+                        paddingRight: "20px",
+                      }}
+                      color="secondary"
+                      label="Email"
+                      variant="outlined"
+                      fullWidth
+                      required
+                    />
+                    <TextField
+                      style={{
+                        margin: "10px",
+                        paddingRight: "20px",
+                      }}
+                      color="secondary"
+                      label="Message"
+                      variant="outlined"
+                      multiline
+                      rows={5}
+                      fullWidth
+                    />
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      // color="secondary"
+                      style={{
+                        alignSelf: "center",
+                        borderRadius: "20px",
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                        textTransform: "capitalize",
+                        backgroundColor: "#f17187",
+                        color: "white",
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  </form>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default contactUs
+export default contactUs;
